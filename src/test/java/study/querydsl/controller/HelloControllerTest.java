1631dfd14f8726be84fee9f3bcbf07150a75e067
mitsui3point <rladbsgh0331@gmail.com>
2022-12-28 12:16:08 +0900

    feat(): add initial settings

---
+++ src/test/java/study/querydsl/controller/HelloControllerTest.java
@@ -0,0 +1,41 @@
+package study.querydsl.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+public class HelloControllerTest {
+    private MockMvc mvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private HelloController helloController;
+
+    @BeforeEach
+    void setUp() {
+//        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        mvc = MockMvcBuilders.standaloneSetup(helloController).build();
+    }
+
+    @Test
+    void helloTest() throws Exception {
+        //given
+
+        //when
+        ResultActions perform = mvc.perform(get("/hello"));
+        //then
+        perform.andDo(print()).andExpect(status().isOk());
+    }
+}
