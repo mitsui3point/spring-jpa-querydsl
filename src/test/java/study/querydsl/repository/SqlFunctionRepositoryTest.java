eadb57d6446a52f1b9d9e76f57e7c63bc6febe23
mitsui3point <rladbsgh0331@gmail.com>
2023-01-03 18:50:47 +0900

    feat($test): add
    - add SQL function 호출하기
    : SQL function은 JPA와 같이 Dialect에 등록된 내용만 호출할 수 있다.
    
    - member M으로 변경하는 replace 함수 사용
    
    - 소문자로 변경해서 비교
    : lower 같은 ansi 표준 함수들은 querydsl이 상당부분 내장하고 있다. 따라서 다음과 같이 처리해도 결과는 같다.
    : .where(member.username.eq(member.username.lower()))

---
+++ src/test/java/study/querydsl/repository/SqlFunctionRepositoryTest.java
@@ -0,0 +1,56 @@
+package study.querydsl.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SqlFunctionRepositoryTest extends TestDataGenerator {
+
+    @Autowired
+    private SqlFunctionRepository sqlFunctionRepository;
+
+    /**
+     * select function('replace', member1.username, 'member', 'M')
+     * from Member member1
+     * <p>
+     * select replace(member0_.username, 'member', 'M') as col_0_0_
+     * from member member0_;
+     * <p>
+     * function {@link org.hibernate.dialect.H2Dialect} 에 등록되어있어야 함
+     * : registerFunction( "replace", new StandardSQLFunction( "replace", StandardBasicTypes.STRING ) );
+     */
+    @Test
+    void sqlFunctionSelectReplaceTest() {
+        //given
+        List<String> expected = Arrays.asList("M1", "M2", "M3", "M4");
+
+        //when
+        List<String> actual = sqlFunctionRepository.sqlFunctionSelectReplace();
+
+        //then
+        actual.forEach(System.out::println);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    /**
+     * select member0_.username as col_0_0_
+     * from member member0_
+     * where member0_.username=lower(member0_.username);
+     */
+    @Test
+    void sqlFunctionWhereLowerTest() {
+        //given
+        List<String> expected = Arrays.asList("member1", "member2", "member3", "member4");
+
+        //when
+        List<String> actual = sqlFunctionRepository.sqlFunctionWhereLower();
+
+        //then
+        actual.forEach(System.out::println);
+        assertThat(actual).isEqualTo(expected);
+    }
+}
